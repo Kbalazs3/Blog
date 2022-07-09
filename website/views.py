9b3cc9994e2b8fd654d4e00fe205ee56d4a52320
Kbalazs3 <kbalazs93@gmail.com>
2022-07-09 23:54:20 +0200

    login endpoint is ready

--- website/views.py
+++ website/views.py
@@ -6,3 +6,8 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def index():
     return render_template("index.html")
+
+
+@views.route("login")
+def login():
+    return render_template("login.html")
