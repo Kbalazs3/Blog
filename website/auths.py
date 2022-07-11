e6e37354857507d552d866cc66f78cd02201fe3e
Kbalazs3 <kbalazs93@gmail.com>
2022-07-12 00:29:24 +0200

    sign up post request, login postrequest are began

--- website/auths.py
+++ website/auths.py
@@ -1,16 +1,18 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 
 auths = Blueprint("auths", __name__)
 
 
-@auths.route("/login")
+@auths.route("/login", methods=["GET", "POST"])
 def log_in():
     return render_template("login.html")
 
 
-@auths.route("/sign-up")
+@auths.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    sign_up_email = request.form.get("sign_up_email")
+    print(sign_up_email)
     return render_template("signup.html")
 
 
